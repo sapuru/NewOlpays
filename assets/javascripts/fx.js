2e0a134fed199a358f9b916d31af93c6d2e9f531
dstocco <daniel.stocco@gmail.com>
2017-10-10 11:54:58 -0300

    OA-1292: code refactor

--- assets/javascripts/fx.js
+++ assets/javascripts/fx.js
@@ -98,14 +98,14 @@ function resetContanctFormDisplay() {
 $(document).on('show.bs.modal', function (event) {
     if (!event.relatedTarget) {
         $('.modal').not(event.target).modal('hide');
-    };
+    }
     if ($(event.relatedTarget).parents('.modal').length > 0) {
         $(event.relatedTarget).parents('.modal').modal('hide');
-    };
+    }
 });
 
 $(document).on('shown.bs.modal', function (event) {
     if ($('body').hasClass('modal-open') == false) {
         $('body').addClass('modal-open');
-    };
+    }
 });
